2f89e2cdfe93e2998e18d1d43c4e253346db7f6c
vanGriekenT <leda.vangrieken61802@ucaldas.edu.co>
2025-05-27 17:38:25 -0500

    Se crea clase Ingeniero y Administrador

---
+++ Administrador.java
@@ -0,0 +1,6 @@
+public class Administrador extends Ingeniero{
+    public Administrador(String nombre, int documento, String nickname, String cargo) {
+        super(nombre, documento, nickname, "Administrador"); // Llama al constructor de la superclase Ingeniero
+    }
+
+}
---
+++ Ingeniero.java
@@ -0,0 +1,15 @@
+public class Ingeniero {
+    String nombre;
+    int documento;
+    String nickname;
+    String cargo;
+
+    // Constructor
+    public Ingeniero(String nombre, int documento, String nickname, String cargo) {
+        this.nombre = nombre;
+        this.documento = documento;
+        this.nickname = nickname;
+        this.cargo = cargo;
+    }
+    // Métodos para acceder y modificar los atributos (getters y setters)
+}
