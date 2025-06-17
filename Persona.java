63b3e02949d69a4851a15a970471dd01b60e5e04
Antoncard <antonio.cardona60111@ucaldas.edu.co>
2025-06-17 15:37:27 -0500

    Update and rename Ingeniero.java to Persona.java

---
+++ Persona.java
@@ -0,0 +1,21 @@
+public class Persona {
+
+    //clase persona
+    String nombre;
+    int cedula;
+    String nickname;
+    String cargo;
+
+    //constructores
+ public Persona (String Nombre, int cedula, String nickname, String cargo){
+     
+     this.nombre = nombre;
+     this.cedula = cedula;
+     this.nickname = nickname;
+     this.cargo = cargo;
+     
+ }
+    
+      
+}
+
