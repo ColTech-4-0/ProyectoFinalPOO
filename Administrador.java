c10581ff2317092d11d83db24424457e40572c32
Antoncard <antonio.cardona60111@ucaldas.edu.co>
2025-06-17 15:38:34 -0500

    Se cambio ingeniero por administrador Administrador.java

--- Administrador.java
+++ Administrador.java
@@ -1,4 +1,4 @@
-public class Administrador extends Ingeniero{
+public class Administrador extends Persona{
     public Administrador(String nombre, int documento, String nickname, String cargo) {
         super(nombre, documento, nickname, "Administrador"); // Llama al constructor de la superclase Ingeniero
     }
