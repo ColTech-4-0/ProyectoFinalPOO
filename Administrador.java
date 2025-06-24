1b044daa7df76ff0fe9838fec542d0556c13ec81
Leda van Grieken <leda.vangrieken61802@ucaldas.edu.co>
2025-06-24 00:10:33 -0500

    Delete Administrador.java
    Eliminando archivos del proyecto anterior para reiniciar el repositorio sin errores

--- Administrador.java
+++
@@ -1,6 +0,0 @@
-public class Administrador extends Persona{
-    public Administrador(String nombre, int documento, String nickname, String cargo) {
-        super(nombre, documento, nickname, "Administrador"); // Llama al constructor de la superclase Ingeniero
-    }
-
-}
