c2877327c2c659d0f707e3f5cd4c0a29d424d38b
vanGriekenT <leda.vangrieken61802@ucaldas.edu.co>
2025-06-24 12:16:39 -0500

    Archivos corregidos

---
+++ Documentos/CURSOS Y BECAS/INGENIERIA INFORMATICA/PROGRAMACION ORIENTADA A OBJETOS/PROYECTO FINAL/PROYECTO FINAL/GestorMantenimiento/src/main/java/com/coltech4/gestormantenimiento/JefeEnfermeria.java
@@ -0,0 +1,14 @@
+package com.coltech4.gestormantenimiento;
+
+/**
+ *
+ * @author Usuario
+ */
+public class JefeEnfermeria extends Persona {
+    
+    public JefeEnfermeria(String nombre, int documento, String nickname, String cargo) {
+        super(nombre, documento, nickname, "Jefe de Enfermería");
+        
+    }
+    
+}
