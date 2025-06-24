b9d647e7856ed9b147114c046f4b60c52e5a8ff2
Leda van Grieken <leda.vangrieken61802@ucaldas.edu.co>
2025-06-24 00:10:56 -0500

    Delete EnfermeraJefe.java
    Eliminando archivos del proyecto anterior para reiniciar el repositorio

--- EnfermeraJefe.java
+++
@@ -1,8 +0,0 @@
-
-public class EnfermeraJefe extends Persona {
-    
-    public EnfermeraJefe(String Nombre, int cedula, String nickname, String cargo) {
-        super(Nombre, cedula, nickname, cargo);
-    }
-    
-}
